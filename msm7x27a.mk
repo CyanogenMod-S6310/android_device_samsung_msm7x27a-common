9336187014d8553e8c05179c17b85134a1e6208c
NV-Dev <alfredomartinez_1423@hotmail.com>
2016-07-09 21:40:43 -0400

    msm7x27a: Set max background services to 8
    Signed-off-by: NV-Dev <alfredomartinez_1423@hotmail.com>

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -236,6 +236,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     mm.enable.smoothstreaming=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.max_starting_bg=8
+
 $(call inherit-product, build/target/product/full.mk)
 $(call inherit-product, device/common/gps/gps_eu_supl.mk)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
